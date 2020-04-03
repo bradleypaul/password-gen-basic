a4179e709652467fac56670b053652a1a1a5654c
Paul Bradley <bradleypauld@gmail.com>
2020-04-03 12:13:12 -0500

    add enter press listener

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Password generator
 
 Password generator site that creates passwords based on criteria supplied by users
+
+## Deployment
+
+GitHub pages live site: <https://bradleypaul.github.io/password-gen-js>
+
+## Technologies
+
+HTML, CSS, JavaScript
--- docs/script.js
+++ docs/script.js
@@ -7,33 +7,33 @@ const specialCharacters = ` !"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~`;
 // prompt user and generate password
 function generatePassword() {
   const length = window.prompt("Provide password length:");
-  if(length >= 8 && length <= 128) {
+  if (length >= 8 && length <= 128) {
     const includeLowercase = confirm("Include lowercase letters?");
     const includeUppercase = confirm("Include uppercase letters?");
     const includeNumbers = confirm("Include numerical characters?");
     const includeSpecialCharacters = confirm("Include special characters?");
-    if(includeLowercase || includeUppercase || includeNumbers || includeSpecialCharacters ) {
+    if (includeLowercase || includeUppercase || includeNumbers || includeSpecialCharacters) {
       let wordbank = '';
-      if(includeLowercase) {
+      if (includeLowercase) {
         wordbank += lowercase;
       }
 
-      if(includeUppercase) {
+      if (includeUppercase) {
         wordbank += uppercase;
       }
 
-      if(includeNumbers) {
+      if (includeNumbers) {
         wordbank += numerals;
       }
 
-      if(includeSpecialCharacters) {
+      if (includeSpecialCharacters) {
         wordbank += specialCharacters;
       }
 
       let password = "";
-      for(let i = 0; i < length; i++) {
+      for (let i = 0; i < length; i++) {
         const random = Math.floor(Math.random() * wordbank.length);
-        password += wordbank[random];  
+        password += wordbank[random];
       }
 
       return password;
@@ -58,7 +58,7 @@ function writePassword() {
   const password = generatePassword();
 
   // make sure password isn't falsy. undefined mainly.
-  if(password) {
+  if (password) {
     document.querySelector("#password").value = password;
   }
 
@@ -66,3 +66,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add event listener for user pressing enter key
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    generateBtn.click();
+  }
+});
